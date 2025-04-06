96c0de72a155d304e2479265d6a93f7fe4d1fcb1
ParnianKhaleghi <parniankhaleghi94@gmail.com>
2025-04-06 11:25:05 +0330

    refactor(utils): change import libraries

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from config import get_web_url
 
 
-html = urlopen('https://myanimelist.net/')
+html = urlopen(get_web_url())
 bs = BeautifulSoup(html.read(), 'html.parser')
 
 print(bs.h1)
